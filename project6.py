d975699c8e344adabcb41b9b96dc92b333a1658d
Zach Fudge <zacheryfudge@gmail.com>
2018-01-10 05:22:50 -0700

    added problem descriptions

--- project6.py
+++ project6.py
@@ -1,3 +1,5 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#What is the 10,001st prime number?
 import math
 def isPrime(num):
 	for n in range(2, int(math.ceil(num**0.5)+1)):
